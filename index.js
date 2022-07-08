e7d99c50904e7b2cfc8e3ffb768381d944aac524
Gina Beth Russell <ginabeth.russell@xometry.com>
2022-07-08 11:59:08 -0400

    changes port to use variable

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const server = require("./api/server.js");
 // Heroku will have the PORT environment variable set
 const port = process.env.PORT || 5000;
 
-server.listen(4000, () => {
+server.listen(port, () => {
   console.log(
     `\n*** Next Meal Service Server Running on http://localhost:${port} ***\n`
   );
