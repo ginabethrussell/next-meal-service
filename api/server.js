d4e5e42c33b54d74de5ea11ed9f0847204c570c1
Gina Beth Russell <ginabeth.russell@xometry.com>
2022-07-08 12:31:49 -0400

    heroku deploy debugging

--- api/server.js
+++ api/server.js
@@ -1,22 +1,11 @@
 const express = require("express");
-const helmet = require("helmet");
 
 const server = express();
 
-server.use(helmet());
 server.use(express.json());
 
 server.get("/", (req, res) => {
   res.status(200).send("Welcome to Next Meal Service");
-});
-
-server.get("/meal", auth, (req, res) => {
-  console.log("Getting a meal...");
-  res.send("Here's your next meal suggestion");
-});
-
-server.use(function (req, res) {
-  res.status(404).send("Oops that resource is not found!");
 });
 
 module.exports = server;
--- index.js
+++ index.js
@@ -1,10 +1,12 @@
-require("dotenv").config();
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
 
 const server = require("./api/server.js");
 
 // Heroku will have the PORT environment variable set
-const port = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 
-server.listen(port, () => {
-  console.log(`\n*** Next Meal Service Server Running on port ${port} ***\n`);
+server.listen(PORT, () => {
+  console.log(`\n*** Next Meal Service Server Running on port ${PORT} ***\n`);
 });
