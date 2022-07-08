4e456349a71ced9b6ad594c6e5b6d9b6e91770a4
Gina Beth Russell <ginabeth.russell@xometry.com>
2022-07-08 11:15:43 -0400

    set up initial server

---
+++ api/server.js
@@ -0,0 +1,49 @@
+const express = require("express");
+
+const server = express();
+
+function logger(req, res, next) {
+  console.log(
+    `[${new Date().toISOString()}] ${req.method} to ${req.url} from ${req.get(
+      "Origin"
+    )}`
+  );
+
+  next();
+}
+
+function logServerStatus(req, res, next) {
+  console.log("Next Meal Service up and running...");
+
+  next();
+}
+
+function auth(req, res, next) {
+  if (req.url === "/meal") {
+    next();
+  } else {
+    res.status(403).send("Not authorized!");
+  }
+}
+
+server.use(express.json());
+server.use(logServerStatus);
+server.use(logger);
+server.get("/", (req, res) => {
+  res.status(200).send("Welcome to Next Meal Service");
+});
+
+server.get("/meal", auth, (req, res) => {
+  console.log("Getting a meal...");
+  res.send("Here's your next meal suggestion");
+});
+
+server.get("/authcheckfails", auth, (req, res) => {
+  res.send("auth is not working");
+});
+
+server.use(function (req, res) {
+  res.status(404).send("Oops that resource is not found!");
+});
+
+module.exports = server;
---
+++ index.js
@@ -0,0 +1,12 @@
+require("dotenv").config();
+
+const server = require("./api/server.js");
+
+// Heroku will have the PORT environment variable set
+const port = process.env.PORT || 5000;
+
+server.listen(4000, () => {
+  console.log(
+    `\n*** Next Meal Service Server Running on http://localhost:${port} ***\n`
+  );
+});
