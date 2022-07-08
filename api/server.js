251a39cd417e241463e6df1184c7b9ba4d649d9d
Gina Beth Russell <ginabeth.russell@xometry.com>
2022-07-08 11:51:09 -0400

    simplifies server to fix deployment

--- api/server.js
+++ api/server.js
@@ -3,33 +3,9 @@ const helmet = require("helmet");
 
 const server = express();
 
-function logger(req, res, next) {
-  console.log(
-    `[${new Date().toISOString()}] ${req.method} to ${req.url} from ${req.get(
-      "Origin"
-    )}`
-  );
-
-  next();
-}
-
-function logServerStatus(req, res, next) {
-  console.log("Next Meal Service up and running...");
-
-  next();
-}
-
-function auth(req, res, next) {
-  if (req.url === "/meal") {
-    next();
-  } else {
-    res.status(403).send("Not authorized!");
-  }
-}
 server.use(helmet());
 server.use(express.json());
-server.use(logServerStatus);
-server.use(logger);
+
 server.get("/", (req, res) => {
   res.status(200).send("Welcome to Next Meal Service");
 });
@@ -37,10 +13,6 @@ server.get("/", (req, res) => {
 server.get("/meal", auth, (req, res) => {
   console.log("Getting a meal...");
   res.send("Here's your next meal suggestion");
-});
-
-server.get("/authcheckfails", auth, (req, res) => {
-  res.send("auth is not working");
 });
 
 server.use(function (req, res) {
