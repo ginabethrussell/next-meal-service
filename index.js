14315b393729a7f3b03f4b1912f46937c5b5194b
Gina Beth Russell <ginabeth.russell@xometry.com>
2022-07-08 12:05:49 -0400

    modifies server file for heroku deploy error

--- index.js
+++ index.js
@@ -6,7 +6,5 @@ const server = require("./api/server.js");
 const port = process.env.PORT || 5000;
 
 server.listen(port, () => {
-  console.log(
-    `\n*** Next Meal Service Server Running on http://localhost:${port} ***\n`
-  );
+  console.log(`\n*** Next Meal Service Server Running on port ${port} ***\n`);
 });
